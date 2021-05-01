ea209316bf99c8173b2f79da0ebf5feba10da6b9
Skeleton2636 <46952604+Skeleton2636@users.noreply.github.com>
2021-05-01 20:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     return render_template('home.html')
 
 @app.route('/predict', methods=['POST'])
-def predict():
+def predict1():
 
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
